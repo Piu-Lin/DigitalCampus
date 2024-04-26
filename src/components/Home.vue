c3675f55c1c477b7f8b253c1b626f3a4b2d8b418
Piu-Lin <55540481+Piu-Lin@users.noreply.github.com>
2024-04-26 15:00:35 +0800

    init

---
+++ src/components/Home.vue
@@ -0,0 +1,34 @@
+<script setup>
+</script>
+
+<template>
+  <div>
+    <img id="baseBG" src="/pngs/baseBG.png"/>
+    <img id="titleBoxBG" src="/pngs/TitleBoxBG.png"/>
+    <div> 数字孪生校园</div>
+    <img id="titleShader" src="/pngs/TitleShader.png"/>
+  </div>
+</template>
+
+<style scoped>
+#baseBG{
+    position: absolute;
+    top:0px;
+    left: 0px;
+    height: 100%;
+    width: 100%;
+}
+#titleBoxBG{
+    position: absolute;
+    top:0px;
+    left: 0px;
+    width: 100%;
+}
+#titleShader{
+    position: absolute;
+    top:0px;
+    left: 0px;
+    width: 30%;
+
+}
+</style>
