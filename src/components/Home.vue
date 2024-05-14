eaa81852ee40da7ad88049f94769b688a9333e92
Bam-Li <Li_Li_0505@163.com>
2024-05-14 16:39:35 +0800

    update

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -10,7 +10,48 @@ function switchSubChosen(v){
 </script>
 
 <template>
+<<<<<<< Updated upstream
     <div id="building">
+=======
+    <Connectpxy/>
+    <div class="all">
+        <div id="building">
+        </div>
+        <div>
+            <img id="baseBG" src="/pngs/baseBG.png" />
+            <img id="titleBoxBG" src="/pngs/TitleBoxBG.png" />
+            <div id="titleContent">之江学院数字孪生校园</div>
+            <img id="titleShader" src="/pngs/TitleShader.png" />
+            <div id="subTitleBox">
+
+                <div @click="switchSubChosen(1)" class="subTitleItemBox">
+                    <span class="subTitleContent">场景展示</span>
+                    <img v-if="subChosen == 1" class="subTitleShader" src="/pngs/SubTitleShader.png" />
+                </div>
+                <div @click="switchSubChosen(2)" class="subTitleItemBox">
+                    <span class="subTitleContent">资产管理</span>
+                    <img v-if="subChosen == 2" class="subTitleShader" src="/pngs/SubTitleShader.png" />
+                </div>
+                <div @click="switchSubChosen(3)" class="subTitleItemBox">
+                    <span class="subTitleContent">运营管理</span>
+                    <img v-if="subChosen == 3" class="subTitleShader" src="/pngs/SubTitleShader.png" />
+                </div>
+                <div @click="switchSubChosen(4)" class="subTitleItemBox">
+                    <span class="subTitleContent">监控中心</span>
+                    <img v-if="subChosen == 4" class="subTitleShader" src="/pngs/SubTitleShader.png" />
+                </div>
+            </div>
+            <div class="TimeDateBox">
+                <span class="TimeData">14:36</span>
+                <span class="fengef">｜</span>
+                <span class="datedata">2024年5月9日</span>
+            </div>
+
+        </div>
+        <AssetManage v-if="subChosen == 2" />
+        <OperationManagement v-if="subChosen == 3" />
+        <MonitoringCenter v-if="subChosen == 4" />
+>>>>>>> Stashed changes
     </div>
   <div>
     <img id="baseBG" src="/pngs/baseBG.png"/>
